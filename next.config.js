43b940de6f277c83140c6b543abfdcbe5e23678e
Sotatek-ThanhLe2 <thanh.le2@sotatek.com>
2022-10-31 23:53:42 +0700

    init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'build',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
